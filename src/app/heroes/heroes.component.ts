8b108098607031f1ac214cab635f7d23160000e3
vgl91 <victorguilarte91@gmail.com>
2023-05-30 11:39:48 -0400

    update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';  
-import { HEROES } from '../mock-heros';
+//import { HEROES } from '../mock-heros'; --> sustituido por el hero.service
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,23 @@ import { HEROES } from '../mock-heros';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Winsdtorm' 
-  };
-  
-  //heroes = HEROES;
-  selectedHero: Hero;
+  //heroes= HEROES; --> sustituido por la declaracion heroes: Hero[] = []
+  heroes: Hero[] = [];  
+
+  selectedHero!: Hero;
+
+  constructor(private heroService: HeroService) {} 
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
 
